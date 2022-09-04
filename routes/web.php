1d885e8e314483d03302ee6348b0a54d14436190
keisuke1523 <u8764c28e772tz@gmail.com>
2022-09-05 01:36:01 +0900

    fix

--- routes/web.php
+++ routes/web.php
@@ -84,3 +84,7 @@ Route::get('/response', 'responseController@index');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('login');
+});
